e42875459bd5f6ffa0f1b0c2e17fa28b2a273232
Tony Illenden <130492486+anthony-illenden@users.noreply.github.com>
2024-06-11 11:22:32 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(0, 10, 100)
-y = np.sin(x)
-
-plt.plot(x, y)
-plt.title('Sine Wave')
+x = np.arange(100)
+heights = np.random.rand(100) * 10
+colors = np.random.rand(100) 
+colormaps = ['viridis', 'plasma', 'inferno', 'magma', 'cividis', 'Greys', 'Purples', 'Blues', 'Greens', 'Oranges', 'Reds', 'YlOrBr', 'YlOrRd', 'OrRd', 'PuRd', 'RdPu', 'BuPu', 'GnBu', 'PuBu', 'YlGnBu', 'PuBuGn', 'BuGn', 'YlGn']
+cmap = np.random.choice(colormaps)
+plt.bar(x, heights, color=plt.get_cmap(cmap)(colors))
+plt.title('Random Bar Graph')
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
-
 plt.savefig('output/plot.png')
